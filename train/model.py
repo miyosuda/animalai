8bdfb907e589a4fec5ebccc8c3341f8c97972994
kosuke miyoshi <miyoshi@narr.jp>
2019-09-13 04:34:54 +0900

    [wip] implementing allocentric model

--- train/model.py
+++ train/model.py
@@ -8,12 +8,13 @@ class AllocentricModel(object):
         self.step_size = tf.placeholder(tf.float32, [1])
         
         with tf.variable_scope("model", reuse=reuse) as scope:
-            self.state_input = tf.placeholder("float", [None, seq_length, 84, 84, 3])
+            self.state_input  = tf.placeholder("float", [None, seq_length, 84, 84, 3])
             self.action_input = tf.placeholder("float", [None, seq_length, 2, 2])
-
-            state_input_reshaped = tf.reshape(self.state_input, [-1, 84, 84, 3])
+            
+            # batch * seq_lengthをまとめる
+            state_input_reshaped  = tf.reshape(self.state_input,  [-1, 84, 84, 3])
             action_input_reshaped = tf.reshape(self.action_input, [-1, 4])
-
+            
             with tf.variable_scope("conv", reuse=reuse) as scope:
                 conv1 = tf.layers.conv2d(state_input_reshaped,
                                          filters=16,
@@ -42,9 +43,10 @@ class AllocentricModel(object):
                 # (-1, 260)
                 
                 self.cell = tf.contrib.rnn.BasicLSTMCell(256, state_is_tuple=True)
-                
-                self.initial_lstm_state0 = tf.placeholder(tf.float32, [1, 256])
-                self.initial_lstm_state1 = tf.placeholder(tf.float32, [1, 256])
+
+                # TODO: バッチサイズの扱い
+                self.initial_lstm_state0 = tf.placeholder(tf.float32, [batch_size, 256])
+                self.initial_lstm_state1 = tf.placeholder(tf.float32, [batch_size, 256])
                 
                 self.initial_lstm_state = tf.contrib.rnn.LSTMStateTuple(
                     self.initial_lstm_state0,
@@ -52,21 +54,40 @@ class AllocentricModel(object):
                 
                 # TODO: バッチサイズの扱い
                 lstm_input_reshaped = tf.reshape(lstm_input, [batch_size, -1, 260])
-
-                #step_size = tf.shape(conv2_flat)[:1]
-
+                # (batch_size, -1, 260)
+                
                 lstm_outputs, lstm_state = tf.nn.dynamic_rnn(
                     self.cell,
                     lstm_input_reshaped,
                     initial_state = self.initial_lstm_state,
-                    #sequence_length = step_size,
-                    sequence_length = seq_length, # TODO: 扱い
                     time_major = False,
                     scope = scope) 
-                # (batch_size, unroll_step, 260)
+                # (batch_size, unroll_step, 256)
+                # ((batch_size, 256), (batch_size, 256))
                 
-                """
                 lstm_outputs = tf.reshape(lstm_outputs, [-1,256])
-                #(1,unroll_step,256) for back prop, (1,1,256) for forward prop.
-                """
+                # (batch_size * unroll_step, 256)
                 
+                fc_out1 = tf.layers.dense(lstm_outputs,
+                                          256,
+                                          activation=tf.nn.relu,
+                                          name="fc_out1")
+                self.output = tf.layers.dense(fc_out1,
+                                              5,
+                                              activation=tf.nn.tanh, # -1~1にする
+                                              name="fc_out2")
+                # (batch_size * unroll_step, 5)
+                
+        def prepare_loss(self):
+            with tf.variable_scope("loss") as scope:
+                # x,y,zを-1~1にnormalizeしたinput
+                self.postion_input = tf.placeholder("float", [None, seq_length, 3])
+                # angleのcos, sinを入力
+                self.angle_input   = tf.placeholder("float", [None, seq_length, 2])
+                
+                target_inputs = tf.concat([self.postion_input, self.angle_input], 2)
+                # (batch_size, seq_length, 5)
+                target_inputs_reshaped = tf.reshape(target_inputs, [-1, 5])
+                # (batch_size * seq_length, 5)
+                
+                self.loss = tf.nn.l2_loss(target_inputs_reshaped - self.output)
--- train/model_test.py
+++ train/model_test.py
@@ -8,8 +8,10 @@ class AllocentricModelTest(unittest.TestCase):
     def test_init(self):
         seq_length = 20
         batch_size = 5
+        
         model = AllocentricModel(seq_length, batch_size)
-
-
+        model.prepare_loss()
+        model_reused = AllocentricModel(seq_length, 1, reuse=True)
+        
 if __name__ == '__main__':
     unittest.main()
