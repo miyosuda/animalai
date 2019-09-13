61ec7cbdf718f45859e6b55a864d842b6cf80769
kosuke miyoshi <miyoshi@narr.jp>
2019-09-13 16:27:46 +0900

    add velocity for the model input

--- train/model.py
+++ train/model.py
@@ -5,15 +5,19 @@ import numpy as np
 
 class AllocentricModel(object):
     def __init__(self, seq_length, batch_size, reuse=False):
+        self.seq_length = seq_length
+        
         self.step_size = tf.placeholder(tf.float32, [1])
         
         with tf.variable_scope("model", reuse=reuse) as scope:
-            self.state_input  = tf.placeholder("float", [None, seq_length, 84, 84, 3])
-            self.action_input = tf.placeholder("float", [None, seq_length, 2, 2])
+            self.state_input    = tf.placeholder("float", [None, seq_length, 84, 84, 3])
+            self.action_input   = tf.placeholder("float", [None, seq_length, 2, 2])
+            self.velocity_input = tf.placeholder("float", [None, seq_length, 2, 3])
             
             # batch * seq_lengthをまとめる
-            state_input_reshaped  = tf.reshape(self.state_input,  [-1, 84, 84, 3])
-            action_input_reshaped = tf.reshape(self.action_input, [-1, 4])
+            state_input_reshaped    = tf.reshape(self.state_input,  [-1, 84, 84, 3])
+            action_input_reshaped   = tf.reshape(self.action_input, [-1, 4]) # 2x2の部分もまとめる
+            velocity_input_reshaped = tf.reshape(self.velocity_input, [-1, 3])
             
             with tf.variable_scope("conv", reuse=reuse) as scope:
                 conv1 = tf.layers.conv2d(state_input_reshaped,
@@ -39,8 +43,8 @@ class AllocentricModel(object):
                                       activation=tf.nn.relu,
                                       name="fc1")
                 
-                lstm_input = tf.concat([fc1, action_input_reshaped], 1)
-                # (-1, 260)
+                lstm_input = tf.concat([fc1, action_input_reshaped, velocity_input_reshaped], 1)
+                # (-1, 263)
                 
                 self.cell = tf.contrib.rnn.BasicLSTMCell(256, state_is_tuple=True)
 
@@ -53,8 +57,8 @@ class AllocentricModel(object):
                     self.initial_lstm_state1)
                 
                 # TODO: バッチサイズの扱い
-                lstm_input_reshaped = tf.reshape(lstm_input, [batch_size, -1, 260])
-                # (batch_size, -1, 260)
+                lstm_input_reshaped = tf.reshape(lstm_input, [batch_size, -1, 263])
+                # (batch_size, -1, 263)
                 
                 lstm_outputs, lstm_state = tf.nn.dynamic_rnn(
                     self.cell,
@@ -78,16 +82,16 @@ class AllocentricModel(object):
                                               name="fc_out2")
                 # (batch_size * unroll_step, 5)
                 
-        def prepare_loss(self):
-            with tf.variable_scope("loss") as scope:
-                # x,y,zを-1~1にnormalizeしたinput
-                self.postion_input = tf.placeholder("float", [None, seq_length, 3])
-                # angleのcos, sinを入力
-                self.angle_input   = tf.placeholder("float", [None, seq_length, 2])
+    def prepare_loss(self):
+        with tf.variable_scope("loss") as scope:
+            # x,y,zを-1~1にnormalizeしたinput
+            self.postion_input = tf.placeholder("float", [None, self.seq_length, 3])
+            # angleのcos, sinを入力
+            self.angle_input   = tf.placeholder("float", [None, self.seq_length, 2])
                 
-                target_inputs = tf.concat([self.postion_input, self.angle_input], 2)
-                # (batch_size, seq_length, 5)
-                target_inputs_reshaped = tf.reshape(target_inputs, [-1, 5])
-                # (batch_size * seq_length, 5)
-                
-                self.loss = tf.nn.l2_loss(target_inputs_reshaped - self.output)
+            target_inputs = tf.concat([self.postion_input, self.angle_input], 2)
+            # (batch_size, seq_length, 5)
+            target_inputs_reshaped = tf.reshape(target_inputs, [-1, 5])
+            # (batch_size * seq_length, 5)
+
+            self.loss = tf.nn.l2_loss(target_inputs_reshaped - self.output)
