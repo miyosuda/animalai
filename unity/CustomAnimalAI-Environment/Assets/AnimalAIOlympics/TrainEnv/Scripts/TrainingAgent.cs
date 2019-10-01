e994970efd5a1a381da9e2d87bed26619f5dfb6a
kosuke miyoshi <miyoshi@narr.jp>
2019-10-01 23:07:56 +0900

    add LIDAR output for object scanning

--- unity/CustomAnimalAI-Environment/Assets/AnimalAIOlympics/TrainEnv/Scripts/TrainingAgent.cs
+++ unity/CustomAnimalAI-Environment/Assets/AnimalAIOlympics/TrainEnv/Scripts/TrainingAgent.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 using MLAgents;
 using PrefabInterface;
+using System.Collections.Generic;
 
 public class TrainingAgent : Agent, IPrefab
 {
@@ -48,6 +49,9 @@ public class TrainingAgent : Agent, IPrefab
     private Color[] _allBlackImage;
     private PlayerControls _playerScript;
 
+    private LIDAR _lidar = new LIDAR();
+
+
     public override void InitializeAgent()
     {
         _area = GetComponentInParent<TrainingArea>();
@@ -67,6 +71,10 @@ public class TrainingAgent : Agent, IPrefab
 
         float rotation = transform.eulerAngles.y;
         AddVectorObs(rotation);
+
+        // Lidarでスキャンした結果をvector observationに追加
+        _lidar.Process(transform);
+        AddVectorObs(_lidar.Result);
     }
 
     public override void AgentAction(float[] vectorAction, string textAction)
@@ -166,5 +174,130 @@ public class TrainingAgent : Agent, IPrefab
     public override bool LightStatus()
     {
         return _area.UpdateLigthStatus(GetStepCount());
+    }
+
+    public class LIDAR 
+    {
+        private string[] targetNames = {
+            "CylinderTunnel(Clone)",
+            "CylinderTunnelTransparent(Clone)",
+            "Ramp(Clone)",
+            "Wall(Clone)",
+            "WallTransparent(Clone)",
+            "Cardbox1(Clone)",
+            "Cardbox2(Clone)",
+            "LObject(Clone)",
+            "LObject2(Clone)",
+            "UObject(Clone)",
+            "BadGoal(Clone)",
+            "BadGoalBounce(Clone)",
+            "DeathZone(Clone)",
+            "GoodGoal(Clone)",
+            "GoodGoalBounce(Clone)",
+            "GoodGoalMulti(Clone)",
+            "GoodGoalMultiBounce(Clone)",
+            "HotZone(Clone)",
+            "WallOut1",
+            "WallOut2",
+            "WallOut3",
+            "WallOut4",
+        };
+        
+        private Dictionary<string, int> targetMap = new Dictionary<string, int>();
+
+        private int[]   resultIds       = new int[5];
+        private float[] resultDistances = new float[5];
+        private float[] result          = new float[10]; // idとdistanceを合わせたもの
+
+        public float[] Result {
+            get {
+                // 前半にID, 後半にdistanceを入れて合わせたものとして返す.
+                for(int i=0; i<resultIds.Length; ++i) {
+                    result[i] = (float)resultIds[i];
+                    result[i+resultIds.Length] = resultDistances[i];
+                }
+                return result;
+            }
+        }
+        
+        public LIDAR()
+        {
+            for(int i=0; i<targetNames.Length; ++i) {
+                string targetName = targetNames[i];
+                targetMap.Add(targetName, i);
+            }
+
+            ClearResult();
+        }
+
+        private string GetTargetName(Collider collided)
+        {
+            string name = collided.gameObject.name;
+            if( name.StartsWith("Cube") || name == "RampFBX" ) {
+                // LObjectとRampの場合はTopの1階層下にColliderが来るので、その上のObjectに
+                // 欲しいオブジェクト名が入っているので例外的に処理する
+                if( collided.gameObject.transform.parent != null ) {
+                    name = collided.gameObject.transform.parent.gameObject.name;
+                }
+            }
+            return name;
+        }
+
+        private Vector3 GetDirectionVec(Transform transform, float angleDegree) {
+            float angleRad = Mathf.Deg2Rad * angleDegree;
+            float c = Mathf.Cos(angleRad);
+            float s = Mathf.Sin(angleRad);
+            Vector3 v = new Vector3(s, 0.0f, c);
+            Vector3 direction = transform.TransformDirection(v);
+            return direction;
+        }
+
+        private void ProcessSub(int index, Transform transform) {
+            // -20度〜20度まで10度きざみでスキャンする
+            float angleDegree = -20.0f + 10.0f * index;
+            
+            Vector3 origin = transform.position;
+            Vector3 direction = GetDirectionVec(transform, angleDegree);
+            
+            int targetId = -1;
+            float targetDistance = -1.0f;
+            
+            RaycastHit hit;
+            if( Physics.Raycast(origin, direction, out hit) ) {
+                //Debug.DrawRay(transform.position, direction * hit.distance, Color.yellow);
+                targetDistance = hit.distance;
+                
+                Collider collided = hit.collider;
+                string targetName = GetTargetName(collided);
+                if( targetMap.ContainsKey(targetName) ) {
+                    targetId = targetMap[targetName];
+                }
+                if( targetId == -1 ) {
+                    Debug.Log("target name=" + targetName + " id=" + targetId);
+                }
+            }
+            // 見えない外壁が高いところまであるので、Raycastが失敗することは実際には無い
+
+            resultIds[index] = targetId;
+            resultDistances[index] = targetDistance;
+        }
+
+        private void ClearResult()
+        {
+            for(int i=0; i<resultIds.Length; ++i) {
+                resultIds[i] = -1;
+                resultDistances[i] = -1.0f;
+            }
+        }
+        
+        public void Process(Transform transform)
+        {
+            ClearResult();
+            
+            // -20度〜20度まで10度きざみでスキャンする
+            for(int i=0; i<5; ++i ) {
+                ProcessSub(i, transform);
+            }
+        }
     }
 }
